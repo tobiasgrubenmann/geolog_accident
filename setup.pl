b60ea8f35b5f839e22f5f1115bd2296d2305d660
Guenter Kniesel <gk@cs.uni-bonn.de>
2021-05-04 20:50:14 +0200

    RENAMINGS

--- setup.pl
+++ setup.pl
@@ -20,28 +20,28 @@ between_tolerance(5).
 
 
 
-designated:designated_name("accidents").
-designated:designated_id("accidents", "accident_id").
+designated:designated_relation("accidents").
+designated:relation_key("accidents", "accident_id").
 
-designated:designated_name("transport").
-designated:designated_id("transport", "osm_id").
+designated:designated_relation("transport").
+designated:relation_key("transport", "osm_id").
 
-designated:designated_name("traffic").
-designated:designated_id("traffic", "osm_id").
+designated:designated_relation("traffic").
+designated:relation_key("traffic", "osm_id").
 
-designated:designated_name("pofw").
-designated:designated_id("pofw", "osm_id").
+designated:designated_relation("pofw").
+designated:relation_key("pofw", "osm_id").
 
-designated:designated_name("pois").
-designated:designated_id("pois", "osm_id").
+designated:designated_relation("pois").
+designated:relation_key("pois", "osm_id").
 
-designated:designated_name("railways").
-designated:designated_id("railways", "osm_id").
+designated:designated_relation("railways").
+designated:relation_key("railways", "osm_id").
 
-designated:designated_name("roads").
-designated:designated_id("roads", "osm_id").
+designated:designated_relation("roads").
+designated:relation_key("roads", "osm_id").
 
-designated:designated_name("buildings").
-designated:designated_id("buildings", "osm_id").
+designated:designated_relation("buildings").
+designated:relation_key("buildings", "osm_id").
 
 ?- start_geolog('C:/Users/Tobias/AppData/Roaming/ESRI/Desktop10.8/ArcCatalog/geolog.sde').
